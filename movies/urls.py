f3cfc7f4617b785441a047a6b9a85bf62f15d87a
brantbecker23 <147889143+brantbecker23@users.noreply.github.com>
2025-01-30 11:51:34 -0500

    Added database, changed movie list and movie individual to pull from database, and added movie search implementation

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='movies.index'),
     path('<int:id>/', views.show, name='movies.show'),
 ]
