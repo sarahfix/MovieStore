69d2d698159ae2e176c261ce3663461f4132f7d1
sarahfix <sarahfix03@gmail.com>
2025-02-01 13:27:21 -0500

    Updated login/signup functionality

--- home/views.py
+++ home/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 def index(request):
     template_data = {}
     template_data['title'] = 'Movies Store'
-    return render(request, 'home/index.html', {
-        'template_data': template_data})
+    return render(request, 'home/index.html', {'template_data': template_data})
 def about(request):
     template_data = {}
     template_data['title'] = 'About'
