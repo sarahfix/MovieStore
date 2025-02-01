53988bc8e759a2e38cd54cdfe8f92e8376bf4174
brantbecker23 <147889143+brantbecker23@users.noreply.github.com>
2025-02-01 14:14:00 -0500

    Fixed views

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('movies/', include('movies.urls'))
+    path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
