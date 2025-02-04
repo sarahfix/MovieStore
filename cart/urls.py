ac81c9cf21ba0e8e7912dbc61d511e0dcf6e7f25
lulugreco <lcgreco0526@gmail.com>
2025-02-03 23:37:14 -0500

    Update urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('<int:id>/add/', views.add, name='cart.add'),
     path('', views.index, name='cart.index'),
     path('clear/', views.clear, name='cart.clear'),
+    path('purchase/', views.purchase, name=
+         cart.purchase'),
 ]
