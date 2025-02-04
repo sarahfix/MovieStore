4e6fc12a8b1a6654754fa7b806a3eb831cde988c
lulugreco <lcgreco0526@gmail.com>
2025-02-03 23:10:20 -0500

    Update models.py

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from movies.models import Movie
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
@@ -9,4 +10,16 @@ class Order(models.Model):
         on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + ' - ' + self.user.username
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order,
+        on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie,
+        on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.movie.name
+
 # Create your models here.
