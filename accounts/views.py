e7c2e4cd20e8dc39f6acea888553ea6ff3e47416
lulugreco <lcgreco0526@gmail.com>
2025-02-03 23:44:20 -0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login as auth_login, authenticate, logout as aut
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 @login_required
 def logout(request):
